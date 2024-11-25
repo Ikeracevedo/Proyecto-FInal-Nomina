79a683b7e2061bbead00e5d1515d6771d74b52fd
Ikeracevedo <159964318+Ikeracevedo@users.noreply.github.com>
2024-11-25 16:25:33 -0500

    1.0

---
+++ ProyectofFinal/src/Main/MainLiquidarNomina.java
@@ -0,0 +1,10 @@
+package Main;
+
+import controlador.ControllerLiquidarNomina;
+import vista.VistaLiquidarNomina;
+
+public class MainLiquidarNomina {
+	public static void main(String[] args) {
+        ControllerLiquidarNomina cLQ = new ControllerLiquidarNomina(new VistaLiquidarNomina());
+    }
+}
