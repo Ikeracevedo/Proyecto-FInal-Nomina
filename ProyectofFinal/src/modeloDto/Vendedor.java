348f3b1b5aaf60fac0452ae737594044a397f783
Ikeracevedo <159964318+Ikeracevedo@users.noreply.github.com>
2024-11-25 16:32:52 -0500

    1.2

--- ProyectofFinal/src/modeloDto/Vendedor.java
+++ ProyectofFinal/src/modeloDto/Vendedor.java
@@ -4,15 +4,37 @@ import java.io.Serializable;
 
 public class Vendedor extends Empleado implements Serializable  {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private int totalVentas;
 	private double salario;
 	private double comision;
 	private double bonificacion;
+	private double descuento;
 	
+	public Vendedor() {
+	}	
+
+
 	public Vendedor(int identificacion, String nombre, int edad, int anio_ingreso, int totalVetas) {
+		// TODO Auto-generated constructor stub
 		super(identificacion, nombre, edad, anio_ingreso);
 		this.totalVentas = totalVetas;
+		this.descuento = descuento;
 	}
+
+
+	public double getDescuento() {
+		return descuento;
+	}
+
+	public void setDescuento(double descuento) {
+		this.descuento = descuento;
+	}
+
+
 
 	public int getTotalVentas() {
 		return totalVentas;
